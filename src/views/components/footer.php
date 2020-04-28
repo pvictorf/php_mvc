acc148e51d46a32a4cef498bb3845e596261f5bd
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-04-28 18:20:59 -0300

    Add components files

---
+++ src/views/components/footer.php
@@ -0,0 +1,5 @@
+<br/>
+<footer style="position: absolute; bottom: 6px; left: 0; width: 100%;">
+   <center>Todos os Direitos</center>
+</footer>
+</body>
--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,1 +1,10 @@
-<h1>Header</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>MVC V</title>
+</head>
+<body style="min-height: 100vh; margin: 0 16px; position:relative;">
+   <header>MVC V</header>
+<hr/>
