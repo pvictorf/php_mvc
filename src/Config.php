963e8774bdca8969ed901953dca0fdbaf9479089
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-05-01 09:53:51 -0300

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -7,7 +7,7 @@ class Config {
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
     const DB_DATABASE = 'dbdev';
-    CONST DB_USER = 'root';
+    const DB_USER = 'root';
     const DB_PASS = 'rootpass';
 
     const ERROR_CONTROLLER = 'ErrorController';
