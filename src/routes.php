f0e68d562f8ac2b19c17a90b99af506ada1711e2
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-05-06 10:05:33 -0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,4 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
-$router->get('/sobre/{nome}', 'HomeController@sobreP');
+$router->get('/sobre/{nome}', 'HomeController@sobre');
